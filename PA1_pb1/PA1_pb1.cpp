66e7fc9d24d7be36a0617dc2461afaa9e47fcaf2
Lee Easson <ljeasson@gmail.com>
2020-02-12 22:46:09 -0800

    Fixed 2D Gaussian smoothing issue with image padding

--- PA1_pb1/PA1_pb1.cpp
+++ PA1_pb1/PA1_pb1.cpp
@@ -22,6 +22,8 @@ void padd_with_zeros_1D(float* signal, float* padded_signal, int width, int filt
 void padd_with_zeros_2D(int** matrix, int** padded_matrix, int width, int height, int filter_size);
 void apply_gaussian_smoothing_1D(float* signal, int signal_size, float kernel[], int kernel_size, float* output_signal);
 void apply_gaussian_smoothing_2D(int** image, int x_size, int y_size, float** kernel, int kernel_size, int** output_image);
+void apply_gaussian_smoothing_2D_with_1D(int** image, int x_size, int y_size, float** kernel, int kernel_size, int** output_image);
+
 
 void generate_gaussian_kernel_1D(float* kernel, double sigma, int kernel_size)
 {
@@ -115,41 +117,16 @@ void apply_gaussian_smoothing_1D(float* signal, int signal_size, float kernel[],
 		}
 		output_signal[index] = sum;
 	}
-	/*
-	int index;
-	float sum;
-	for (index = 0; index < signal_size - kernel_size; index++)
-	{
-		sum = 0.0;
-		for (int i = 0; i < kernel_size; i++)
-		{
-			sum += signal[index + i] * kernel[i];
-		}
-		output_signal[index] = sum;
-	}
-	*/
 }
 
 void apply_gaussian_smoothing_2D(int** image, int x_size, int y_size, float** kernel, int kernel_size, int** output_image)
 {
-	/*
-	cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 20; j++) {
-			cout << image[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	*/
-
 	for (int index_i = kernel_size / 2; index_i < y_size - (kernel_size / 2); ++index_i)
 	{
 		for (int index_j = kernel_size / 2; index_j < x_size - (kernel_size / 2); ++index_j)
 		{
 			float sum = 0;
 
-			//cout << index_i << " " << index_j << endl;
 			for (int i = -kernel_size / 2; i <= kernel_size / 2; ++i)
 			{
 				for (int j = -kernel_size / 2; j <= kernel_size / 2; ++j)
@@ -158,25 +135,28 @@ void apply_gaussian_smoothing_2D(int** image, int x_size, int y_size, float** ke
 					float coeff = kernel[i + (kernel_size / 2)][j + (kernel_size / 2)];
 
 					sum += data * coeff;
-					//cout << "Data: " << data << "  Coeff: " << coeff << "        Sum: " << sum << endl;
-
 				}
 			}
 
+			output_image[index_i - kernel_size/2][index_j - kernel_size/2] = sum;
+		}
+	}
+}
+
+void apply_gaussian_smoothing_2D_with_1D(int** image, int x_size, int y_size, float kernel[], int kernel_size, int** output_image)
+{
+	for (int index_i = kernel_size / 2; index_i < y_size - (kernel_size / 2); ++index_i)
+	{
+		for (int index_j = kernel_size / 2; index_j < x_size - (kernel_size / 2); ++index_j)
+		{
+			float sum = 0.0;
+			for (int i = kernel_size / 2; i < kernel_size - (kernel_size / 2); ++i)
+			{
+				sum += image[index_i + i][index_j] * kernel[i];
+			}
 			output_image[index_i][index_j] = sum;
 		}
 	}
-
-	/*
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 20; j++) {
-			cout << output_image[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-	cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
-	*/
 }
 
 
@@ -188,6 +168,7 @@ int main()
 
 
 	/* 1D Gaussian Smoothing */
+	/*
 	// Read in Rect_128.txt as a matrix
 	ifstream in("Rect_128.txt");
 	float rect_128[128];
@@ -222,7 +203,7 @@ int main()
 	resize(smoothed_signal, smoothed_signal_resized, cv::Size(640, 240), 0, 0, cv::INTER_AREA);
 	imshow("Rect_128 (Smoothed)", smoothed_signal_resized);
 	//imwrite("Rect_128 (Smoothed).jpg", smoothed_signal_resized);
-
+	*/
 
 	//////////////////////////////////////////////////////
 	/* 2D Gaussian Smooting */
@@ -231,8 +212,16 @@ int main()
 	char name[20] = "lenna.pgm";
 	char outfile[20] = "lenna_smoothed.pgm";
 
-
 	ReadImage(name, &input, x_size, y_size, Q);
+	cout << "Original Image:" << endl;
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			cout << input[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
 
 	// Generate 2D Gaussian kernel
 	float** Gaussian_Kernel_2D;
@@ -240,6 +229,78 @@ int main()
 	for (int i = 0; i < mask_size; i++)
 		Gaussian_Kernel_2D[i] = new float[mask_size];
 	generate_gaussian_kernel_2D(Gaussian_Kernel_2D, sigma, mask_size);
+
+	cout << "2D Gaussian Kernel:" << endl;
+	for (int i = 0; i < mask_size; i++) {
+		for (int j = 0; j < mask_size; j++) {
+			cout << Gaussian_Kernel_2D[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+	// Pad image with zeros
+	int** input_padded;
+	input_padded = new int* [y_size + mask_size - 1];
+	for (int i = 0; i < y_size + mask_size - 1; i++)
+		input_padded[i] = new int[x_size + mask_size - 1];
+	padd_with_zeros_2D(input, input_padded, x_size, y_size, mask_size);
+
+	cout << "Padded Image:" << endl;
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			cout << input_padded[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+	// Apply Gaussian smoothing to image
+	int** input_smoothed;
+	input_smoothed = new int* [y_size];
+	for (int i = 0; i < y_size; i++)
+		input_smoothed[i] = new int[x_size];
+	for (int i = 0; i < y_size; i++)
+		for (int j = 0; j < x_size; j++)
+			input_smoothed[i][j] = 0;
+
+	apply_gaussian_smoothing_2D(input_padded, x_size + mask_size - 1, y_size + mask_size - 1, Gaussian_Kernel_2D, mask_size, input_smoothed);
+	//apply_gaussian_smoothing_2D(input_padded, x_size, y_size, Gaussian_Kernel_2D, mask_size, input_smoothed);
+
+	cout << "Smoothed Image:" << endl;
+	for (int i = 0; i < 25; i++) {
+		for (int j = 0; j < 25; j++) {
+			cout << input_smoothed[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+
+	WriteImage(outfile, input_smoothed, x_size, y_size, Q);
+
+
+	
+	//////////////////////////////////////////////////////
+	/* 2D Gaussian Smooting with 1D Convolution */
+	/*
+	int** input, ** output;
+	int x_size, y_size, Q;
+	char name[25] = "lenna.pgm";
+	char outfile[25] = "lenna_smoothed_1D.pgm";
+	
+	ReadImage(name, &input, x_size, y_size, Q);
+
+	// Generate 1D Gaussian kernel 
+	float Gaussian_Kernel_2D_1D[mask_size];
+	generate_gaussian_kernel_1D(Gaussian_Kernel_2D_1D, sigma, mask_size);
+
+	cout << "1D Gaussian Kernel:  [ ";
+	for (int i = 0; i < mask_size; i++)
+		cout << Gaussian_Kernel_2D_1D[i] << " ";
+	cout << "]" << endl;
 
 	// Pad image with zeros
 	int** input_padded;
@@ -257,15 +318,11 @@ int main()
 	for (int i = 0; i < y_size + mask_size - 1; i++)
 		for (int j = 0; j < x_size + mask_size - 1; j++)
 			input_smoothed[i][j] = 0;
-	//apply_gaussian_smoothing_2D(input_padded, x_size + mask_size - 1, y_size + mask_size - 1, Gaussian_Kernel_2D, mask_size, input_smoothed);
-	apply_gaussian_smoothing_2D(input_padded, x_size, y_size, Gaussian_Kernel_2D, mask_size, input_smoothed);
+
+	apply_gaussian_smoothing_2D_with_1D(input_padded, x_size, y_size, Gaussian_Kernel_2D_1D, mask_size, input_smoothed);
 
 	WriteImage(outfile, input_smoothed, x_size, y_size, Q);
-
-
-	//////////////////////////////////////////////////////
-	/* 2D Gaussian Smooting with 1D Convolution */
-
+	*/
 
 	waitKey(0);
 	return 0;
